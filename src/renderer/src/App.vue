e21e0620e71dc6cdcce92dbce9eb8bd27ad5de67
René Preuß <rene@preuss.io>
2024-12-09 11:01:53 +0100

    Improve responsiveness

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -3,16 +3,16 @@
     class="min-h-full select-none"
     @contextmenu.prevent
   >
-    <AppPopups/>
-    <AppNotifications/>
-    <AppTitlebar/>
+    <AppPopups />
+    <AppNotifications />
+    <AppTitlebar />
 
     <template v-if="store.connected">
       <main class="flex flex-col gap-4 px-8 py-4 h-[calc(100vh-52px)]">
-        <AppInfoBar/>
+        <AppInfoBar />
 
         <div class="flex-auto">
-          <AppGoldenLayout/>
+          <AppGoldenLayout />
         </div>
 
         <footer>
@@ -21,10 +21,13 @@
               class="text-center text-sm text-gray-500 sm:text-left"
             >
               <span class="block sm:inline">
-                &copy; 2024 René Preuß. All rights reserved. Running OBS Websocket
-                {{ store.version.obsWebSocketVersion }} on OBS Studio
-                {{ store.version.obsVersion }} on
-                {{ store.version.platformDescription }}.
+                &copy; 2024 René Preuß. All rights reserved.
+                <span class="hidden xl:inline">
+                  Running OBS Websocket
+                  {{ store.version.obsWebSocketVersion }} on OBS Studio
+                  {{ store.version.obsVersion }} on
+                  {{ store.version.platformDescription }}.
+                </span>
               </span>
             </div>
             <div class="flex gap-2 text-center">
@@ -36,7 +39,7 @@
                 </div>
                 <div>|</div>
                 <div>
-                  Workbench Version: {{ gitHash }}
+                  Version: {{ gitHash }}
                 </div>
               </div>
             </div>
@@ -70,14 +73,14 @@ import AppPopups from './components/organisms/AppPopups.vue'
 import AppNotifications from './components/organisms/AppNotifications.vue'
 import AppTitlebar from './components/organisms/AppTitlebar.vue'
 import AppGoldenLayout from './components/molecules/AppGoldenLayout.vue'
-import {useAppStore} from './store/app'
+import { useAppStore } from './store/app'
 import AppEmptyState from './components/atoms/AppEmptyState.vue'
-import {usePopupStore} from './store/popup'
-import ConnectPopup from "./components/popups/ConnectPopup.vue";
-import {computed} from "vue";
+import { usePopupStore } from './store/popup'
+import ConnectPopup from './components/popups/ConnectPopup.vue'
+import { computed } from 'vue'
 
 const store = useAppStore()
-const {openPopup} = usePopupStore()
+const { openPopup } = usePopupStore()
 
 const transferred = computed(() => {
   return (store.bandwidth.bytesTransferred / 1024 / 1024).toFixed(2)
